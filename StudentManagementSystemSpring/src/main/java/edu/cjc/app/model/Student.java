1a5e1acc5697c11b0dd8d652fb8dadd6d40b1614
pratikshataware <96300978+pratikshataware@users.noreply.github.com>
2025-12-24 14:10:46 +0530

    commmit first

---
+++ StudentManagementSystemSpring/src/main/java/edu/cjc/app/model/Student.java
@@ -0,0 +1,5 @@
+package edu.cjc.app.model;
+
+public class Student {
+
+}
---
+++ StudentManagementSystemSpring/src/test/java/edu/cjc/com/StudentManagementSystemSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cjc.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
